d094cf3db2d2c256938bf19080da1f8d6f542aac
Jeridi Mohamed <74735976+amadich@users.noreply.github.com>
2023-05-25 19:19:01 +0100

    Update getanimes.js

--- models/gets/getanimes.js
+++ models/gets/getanimes.js
@@ -1,12 +1,15 @@
 const animeModel = require("../CreateAnime");
 
 const getanimes = (app) => {
-   app.get("/getanimes" , async (req,res) => {
-
-      const mycatalog = await animeModel.find();
-      res.status(200).json({mycatalog});
-      
-   })
-}
+   app.get("/getanimes", async (req, res) => {
+      try {
+         const mycatalog = await animeModel.find();
+         res.status(200).json({ mycatalog });
+      } catch (error) {
+         console.log("Error in getanimes:", error);
+         res.status(500).json({ message: "An error occurred. Please try again later.", ok: 0 });
+      }
+   });
+};
 
 module.exports = getanimes;
