74353b5cd231ae04e75c5490ba857ce2f9b24ef7
Jeridi Mohamed <74735976+amadich@users.noreply.github.com>
2023-05-25 19:13:23 +0100

    Update c_db.js

--- models/c_db.js
+++ models/c_db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const url_db_local = process.env.DBMONGO;
+
 // Connect to MongoDB
 mongoose.connect(url_db_local, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  connectTimeoutMS: 30000, // Set the connection timeout to 30 seconds
 })
   .then(() => {
     console.log('Connected to MongoDB');
